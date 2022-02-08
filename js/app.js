98d4032479996850b2ad9b0109c7c818a4cea845
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-02-08 09:53:52 +0600

    editing

--- js/app.js
+++ js/app.js
@@ -10,11 +10,24 @@ function scrollToTop() {
 
 // New Arrival
 
-{/* <span class="wish-icon"><i class="fa fa-heart-o"></i></span> */}
-
+{/* <span class="wish-icon"><i class="fa fa-heart-o"></i></span> */ }
+document.g
 
 $(document).ready(function () {
     $(".wish-icon i").click(function () {
-        $(this).toggleClass("fa-heart fa-heart-o");
+        $(this).toggleClass("fas fa-heart");
     });
-});	
+});
+
+
+
+// Slick Carousel 
+$(function () {
+    $('.blog-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<span class="priv-arrow"><i class=" fas fa-angle-left"></i></span>',
+        nextArrow: '<span class="next-arrow"><i class=" fas fa-angle-right"></i></span>'
+    });
+});

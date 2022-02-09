d526270960a64ea3dab6519770b7003c2fd4c049
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-02-09 13:08:33 +0600

    most reviewed section added

--- js/app.js
+++ js/app.js
@@ -15,8 +15,79 @@ const wishIcons = document.getElementsByClassName('wish-icon');
 for (const wishIcon of wishIcons) {
     wishIcon.addEventListener('click', function () {
         wishIcon.innerHTML = `<i class="fas fa-heart"></i>`;
-    })
-}
+    });
+};
+
+
+
+
+
+
+
+
+// Star Rating 
+// document.querySelector('.rating').addEventListener('click', function (e) {
+//     let action = 'add';
+//     for (const span of this.children) {
+//         span.classList[action]('active');
+//         if (span === e.target) action = 'remove';
+//     }
+// });
+
+// const rating = document.getElementsByClassName('rating');
+// const rate = document.querySelectorAll('.rating span');
+// rate.addEventListener('click', function () {
+//     for (const rate of rating) {
+//         console.log('jaka');
+//     }
+// })
+
+
+
+// let stars = document.querySelectorAll('.star');
+// document.querySelector('.star-container').addEventListener('click', starRating);
+// let rating = document.querySelector('.rating');
+
+// function starRating(e) {
+// 	if (e.target.classList[0] == 'star') {
+// 		stars.forEach((star) => star.classList.remove('star__checked'));
+// 		for (i = stars.length - 1, j = 0; i >= 0; i--, j++) {
+// 			if (stars[i] !== e.target) {
+// 				stars[i].classList.add('star__checked');
+// 			} else {
+// 				stars[i].classList.add('star__checked');
+// 				rating.textContent = `${j + 1}/5`;
+// 				break;
+// 			}
+// 		}
+// 	} else {
+// 		stars.forEach((star) => star.classList.remove('star__checked'));
+// 		rating.textContent = `${0}/5`;
+// 	}
+// }
+
+
+
+
+const allstars = document.querySelectorAll('.fa-star')
+
+allstars.forEach(star => {
+    star.onclick = () => {
+        let starlevel = star.getAttribute('data-num');
+        allstars.forEach(el => { //loop through stars again to compare the clicked star to all other stars
+            if (starlevel < el.getAttribute('data-num')) {
+                el.classList.remove('fas');
+                el.classList.add('far');
+            }
+            else {
+                el.classList.remove('far');
+                el.classList.add('fas');
+            }
+        });
+    };
+});
+
+
 
 
 
@@ -56,4 +127,26 @@ $(function () {
             }
         ]
     });
+});
+
+
+
+
+// Animation For 50%
+// When Scroll 
+$(document).ready(function () {
+    new WOW().init();
+});
+
+
+
+// Isotope Carousel for Most Reviewed 
+
+var $grid = $('.mostReviewedProducts').isotope({
+    // options
+});
+// filter items on button click
+$('.mostReviewedLinks').on('click', 'li', function () {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
 });

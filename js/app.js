15ac56e1e359fa9a9b17a888341bd4f38578f0c3
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-02-10 13:50:59 +0600

    updating some problems

--- js/app.js
+++ js/app.js
@@ -1,91 +1,26 @@
 
-// Scroll To Top 
-function scrollToTop() {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    })
-}
-
-
 // New Arrival
+
 // Wish Icons 
 
-const wishIcons = document.getElementsByClassName('wish-icon');
-for (const wishIcon of wishIcons) {
-    wishIcon.addEventListener('click', function () {
-        wishIcon.innerHTML = `<i class="fas fa-heart"></i>`;
+
+
+const faHearts = document.getElementsByClassName('fa-heart');
+
+for (const faHeart of faHearts) {
+    faHeart.addEventListener('click', function () {
+        if (faHeart.className == 'far fa-heart') {
+            faHeart.classList.remove('far', 'fa-heart');
+            faHeart.classList.add('fas', 'fa-heart');
+        }
+        else {
+            faHeart.classList.remove('fas', 'fa-heart');
+            faHeart.classList.add('far', 'fa-heart');
+        }
     });
-};
 
+}
 
-
-
-
-
-
-
-// Star Rating 
-// document.querySelector('.rating').addEventListener('click', function (e) {
-//     let action = 'add';
-//     for (const span of this.children) {
-//         span.classList[action]('active');
-//         if (span === e.target) action = 'remove';
-//     }
-// });
-
-// const rating = document.getElementsByClassName('rating');
-// const rate = document.querySelectorAll('.rating span');
-// rate.addEventListener('click', function () {
-//     for (const rate of rating) {
-//         console.log('jaka');
-//     }
-// })
-
-
-
-// let stars = document.querySelectorAll('.star');
-// document.querySelector('.star-container').addEventListener('click', starRating);
-// let rating = document.querySelector('.rating');
-
-// function starRating(e) {
-// 	if (e.target.classList[0] == 'star') {
-// 		stars.forEach((star) => star.classList.remove('star__checked'));
-// 		for (i = stars.length - 1, j = 0; i >= 0; i--, j++) {
-// 			if (stars[i] !== e.target) {
-// 				stars[i].classList.add('star__checked');
-// 			} else {
-// 				stars[i].classList.add('star__checked');
-// 				rating.textContent = `${j + 1}/5`;
-// 				break;
-// 			}
-// 		}
-// 	} else {
-// 		stars.forEach((star) => star.classList.remove('star__checked'));
-// 		rating.textContent = `${0}/5`;
-// 	}
-// }
-
-
-
-
-const allstars = document.querySelectorAll('.fa-star')
-
-allstars.forEach(star => {
-    star.onclick = () => {
-        let starlevel = star.getAttribute('data-num');
-        allstars.forEach(el => { //loop through stars again to compare the clicked star to all other stars
-            if (starlevel < el.getAttribute('data-num')) {
-                el.classList.remove('fas');
-                el.classList.add('far');
-            }
-            else {
-                el.classList.remove('far');
-                el.classList.add('fas');
-            }
-        });
-    };
-});
 
 
 
@@ -132,11 +67,77 @@ $(function () {
 
 
 
-// Animation For 50%
+
 // When Scroll 
-$(document).ready(function () {
-    new WOW().init();
-});
+
+
+// Animation For 50%
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+// Percent 50%
+
+function percent50() {
+    var reveals = document.querySelectorAll(".percent50");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("percent50Active");
+        } else {
+            reveals[i].classList.remove("percent50Active");
+        }
+    }
+}
+
+window.addEventListener("scroll", percent50);
+
+
+
+// Scroll To Top 
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+// When the user scrolls down 20px from the top of the document, showing button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopBtn.style.display = "block";
+    } else {
+        scrollToTopBtn.style.display = "none";
+    }
+}
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+
+
+
+
 
 
 
@@ -150,3 +151,14 @@ $('.mostReviewedLinks').on('click', 'li', function () {
     var filterValue = $(this).attr('data-filter');
     $grid.isotope({ filter: filterValue });
 });
+
+
+
+
+
+//=========    Cart
+
+const cartButton = document.getElementById('cartButton');
+cartButton.addEventListener('click',function(){
+    window.location.href = 'cart.html';
+})
